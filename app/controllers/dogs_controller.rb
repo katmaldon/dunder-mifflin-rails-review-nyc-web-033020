71ed12ad053f58bc3d0a3c454ff6682e4ce75ed8
github username <github email address>
2020-05-08 13:35:03 -0400

    github test

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,37 @@
 class DogsController < ApplicationController
+
+  def index
+    @dogs = Dog.all
+  end
+
+  def show
+    @dog = Dog.find(params[:id])
+  end
+
+  def new
+    @dog = Dog.new
+  end
+
+  def create
+    @dog = Dog.create(dog_params)
+  end
+
+  def edit
+    @dog = Dog.find(params[:id])
+  end
+
+  def update
+    @dog = Dog.update(dog_params)
+  end
+
+  def destroy
+    @dog = Dog.destroy
+  end
+
+  private
+
+  def dog_params
+    require(:name, :breed, :age).permit(:name, :breed, :age)
+  end
+
 end
--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,31 @@
 class EmployeesController < ApplicationController
+
+  def index
+    @employees = Employee.all
+  end
+
+  def show
+    @employee = Employee.find(params[:id])
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
 end
